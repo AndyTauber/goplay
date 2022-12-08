613bf7d8216df75c5406d473b7b4a948e0518d9f
Andy.Tauber <atauber@vmware.com>
2022-12-08 14:40:05 -0500

    init

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string = "Hello, World"
+	var year int = 2022
+	fmt.Println(x, year)
+	var next_year int32 = 2023
+	fmt.Printf("type of x : %T\n", x)
+	fmt.Printf("type of next_year : %T\n", next_year)
+}
